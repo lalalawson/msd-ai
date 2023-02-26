d9f10ca5ab3841a13076f5c6df8923740262dc94
itsonlocalhost <tvrkrishna100@gmail.com>
2023-02-26 12:21:25 +0800

    done

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -207,41 +207,48 @@ function Create() {
                 </div>
               </div>
 
-              {selectedType === 0 && (
-                <div>
-                  <label
-                    htmlFor="raw-text"
-                    className="block text-sm font-medium text-gray-800"
-                  >
-                    Raw Text
-                  </label>
-                  <div className="mt-1">
-                    <textarea
-                      id="raw-text"
-                      name="raw-text"
-                      rows={15}
-                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-600 focus:ring-teal-800 sm:text-sm"
-                      placeholder="Enter the research article's raw text here..."
-                      defaultValue={""}
-                    />
-                  </div>
-                </div>
-              )}
               {selectedType === 1 && (
                 <div>
                   <label className="block text-sm font-medium text-gray-700">
                     Upload a file
                   </label>
-
-                  <div>
-                    <input
-                      type="file"
-                      onChange={handleFileInputChange}
-                      accept=".pdf"
-                    />
+                  <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                    <div className="space-y-1 text-center">
+                      <svg
+                        className="mx-auto h-12 w-12 text-gray-400"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                      <div className="flex text-sm text-gray-600">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white font-medium text-teal-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span>Upload a file</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            className="sr-only"
+                          />
+                        </label>
+                        <p className="pl-1">or drag and drop</p>
+                      </div>
+                      <p className="text-xs text-gray-500">PDF up to 10MB</p>
+                    </div>
                   </div>
                 </div>
               )}
+
               {selectedType === 2 && (
                 <div className="col-span-3 sm:col-span-2">
                   <label
